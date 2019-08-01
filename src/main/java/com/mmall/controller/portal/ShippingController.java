ae7a743748198d8545b8b0d8077b35ae450e41b1
Yifan - Macbook Pro <zyfinori@outlook.com>
2019-08-02 00:29:49 +0800

    Complete shipping module

---
+++ src/main/java/com/mmall/controller/portal/ShippingController.java
@@ -0,0 +1,89 @@
+package com.mmall.controller.portal;
+
+import com.github.pagehelper.PageInfo;
+import com.mmall.common.Const;
+import com.mmall.common.ResponseCode;
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.Shipping;
+import com.mmall.pojo.User;
+import com.mmall.service.IShippingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/shipping/")
+public class ShippingController {
+
+    @Autowired
+    private IShippingService iShippingService;
+
+    // 增加地址
+    // 直接用Spring MVC的对象数据绑定，直接传入Shipping对象，而不单独传入各个参数
+    // Spring MVC会自动把参数赋值给对象
+    @RequestMapping("add.do")
+    @ResponseBody
+    public ServerResponse add(HttpSession session, Shipping shipping) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) { // 用户尚未登录
+            return ServerResponse
+                    .createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iShippingService.add(user.getId(), shipping);
+    }
+
+    // 删除地址
+    @RequestMapping("del.do")
+    @ResponseBody
+    public ServerResponse<String> del(HttpSession session, Integer shippingId) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) { // 用户尚未登录
+            return ServerResponse
+                    .createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iShippingService.del(user.getId(), shippingId);
+    }
+
+    // 更新地址
+    @RequestMapping("update.do")
+    @ResponseBody
+    public ServerResponse update(HttpSession session, Shipping shipping) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) { // 用户尚未登录
+            return ServerResponse
+                    .createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iShippingService.update(user.getId(), shipping);
+    }
+
+    // 查询单个地址
+    @RequestMapping("select.do")
+    @ResponseBody
+    public ServerResponse<Shipping> select(HttpSession session, Integer shippingId) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) { // 用户尚未登录
+            return ServerResponse
+                    .createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iShippingService.select(user.getId(), shippingId);
+    }
+
+    // 查询所有地址
+    @RequestMapping("list.do")
+    @ResponseBody
+    public ServerResponse<PageInfo> list(@RequestParam(value = "pageNum", defaultValue = "1") int pageNum,
+                                         @RequestParam(value = "pageSize", defaultValue = "10")  int pageSize,
+                                         HttpSession session) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) { // 用户尚未登录
+            return ServerResponse
+                    .createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iShippingService.list(user.getId(), pageNum, pageSize);
+    }
+
+}
---
+++ src/main/java/com/mmall/service/impl/ShippingServiceImpl.java
@@ -0,0 +1,79 @@
+package com.mmall.service.impl;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.google.common.collect.Maps;
+import com.mmall.common.ServerResponse;
+import com.mmall.dao.ShippingMapper;
+import com.mmall.pojo.Shipping;
+import com.mmall.service.IShippingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service("iShippingService")
+public class ShippingServiceImpl implements IShippingService {
+
+    @Autowired
+    private ShippingMapper shippingMapper;
+
+    // 增加地址
+    @Override
+    public ServerResponse add(Integer userId, Shipping shipping) {
+        shipping.setUserId(userId);
+        int rowCount = shippingMapper.insert(shipping);
+        if (rowCount > 0) {
+            Map result = Maps.newHashMap();
+            result.put("shippingId", shipping.getId());
+            return ServerResponse.createBySuccess("新建地址成功", result);
+        }
+        return ServerResponse.createByErrorMessage("新建地址失败");
+    }
+
+    // 删除地址
+    @Override
+    public ServerResponse<String> del(Integer userId, Integer shippingId) {
+        // 必须用userId和shippingId都有的方法，以防止横向越权
+        // 若只传入shippingId，则一个用户可以删除其他用户的地址
+        int resultCount = shippingMapper.deleteByShippingIdUserId(userId, shippingId);
+        if (resultCount > 0) {
+            return ServerResponse.createBySuccess("删除地址成功");
+        }
+        return ServerResponse.createByErrorMessage("删除地址失败");
+    }
+
+    // 更新地址
+    @Override
+    public ServerResponse update(Integer userId, Shipping shipping) {
+        shipping.setUserId(userId);
+        int rowCount = shippingMapper.updateByShipping(shipping);
+        if (rowCount > 0) {
+            Map result = Maps.newHashMap();
+            result.put("shippingId", shipping.getId());
+            return ServerResponse.createBySuccess("更新地址成功");
+        }
+        return ServerResponse.createByErrorMessage("更新地址失败");
+    }
+
+    // 查询单个地址
+    @Override
+    public ServerResponse<Shipping> select(Integer userId, Integer shippingId) {
+        Shipping shipping = shippingMapper.selectByShippingIdUserId(userId, shippingId);
+        if (shipping == null) {
+            return ServerResponse.createByErrorMessage("无法查询到该地址");
+        }
+        return ServerResponse.createBySuccess("查询地址成功", shipping);
+    }
+
+    // 查询所有地址
+    @Override
+    public ServerResponse<PageInfo> list(Integer userId, int pageNum, int pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        List<Shipping> shippingList = shippingMapper.selectByUserId(userId);
+        PageInfo pageInfo = new PageInfo(shippingList);
+        return ServerResponse.createBySuccess(pageInfo);
+    }
+
+}
