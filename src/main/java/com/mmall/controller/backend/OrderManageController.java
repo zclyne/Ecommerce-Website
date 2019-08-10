cdc1ae40b232619152c422573a1c581cc921a676
Yifan - Macbook Pro <zyfinori@outlook.com>
2019-08-10 22:11:38 +0800

    Complete order module

--- src/main/java/com/mmall/controller/backend/OrderManageController.java
+++ src/main/java/com/mmall/controller/backend/OrderManageController.java
@@ -7,6 +7,7 @@ import com.mmall.common.ServerResponse;
 import com.mmall.pojo.User;
 import com.mmall.service.IOrderService;
 import com.mmall.service.IUserService;
+import com.mmall.vo.OrderVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +26,7 @@ public class OrderManageController {
     @Autowired
     private IOrderService iOrderService;
 
+    // 后台显示订单列表
     @RequestMapping("list.do")
     @ResponseBody
     public ServerResponse<PageInfo> orderList(HttpSession session,
@@ -37,6 +39,54 @@ public class OrderManageController {
         if (iUserService.checkAdminRole(user).isSuccess()) {
             // 列出所有的订单
             return iOrderService.manageList(pageNum, pageSize);
+        }
+        return ServerResponse.createByErrorMessage("无执行该操作的权限");
+    }
+
+    // 后台查询订单详情
+    @RequestMapping("detail.do")
+    @ResponseBody
+    public ServerResponse<OrderVo> orderList(HttpSession session, Long orderNo) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "用户未登录，请登录管理员");
+        }
+        if (iUserService.checkAdminRole(user).isSuccess()) {
+            // 获取订单详情
+            return iOrderService.manageDetail(orderNo);
+        }
+        return ServerResponse.createByErrorMessage("无执行该操作的权限");
+    }
+
+    // 后台按订单号搜索
+    @RequestMapping("search.do")
+    @ResponseBody
+    public ServerResponse<PageInfo> orderSearch(HttpSession session,
+                                               Long orderNo,
+                                               @RequestParam(value = "pageNum", defaultValue = "1") int pageNum,
+                                               @RequestParam(value = "pageSize", defaultValue = "10") int pageSize) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "用户未登录，请登录管理员");
+        }
+        if (iUserService.checkAdminRole(user).isSuccess()) {
+            // 搜索订单
+            return iOrderService.manageSearch(orderNo, pageNum, pageSize);
+        }
+        return ServerResponse.createByErrorMessage("无执行该操作的权限");
+    }
+
+    // 后台订单发货
+    @RequestMapping("send_goods.do")
+    @ResponseBody
+    public ServerResponse<String> orderSendGoods(HttpSession session, Long orderNo) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "用户未登录，请登录管理员");
+        }
+        if (iUserService.checkAdminRole(user).isSuccess()) {
+            // 订单发货
+            return iOrderService.manageSendGoods(orderNo);
         }
         return ServerResponse.createByErrorMessage("无执行该操作的权限");
     }
--- src/main/java/com/mmall/service/IOrderService.java
+++ src/main/java/com/mmall/service/IOrderService.java
@@ -26,4 +26,10 @@ public interface IOrderService {
 
     ServerResponse<PageInfo> manageList(int pageNum, int pageSize);
 
+    ServerResponse<OrderVo>  manageDetail(Long orderNo);
+
+    ServerResponse<PageInfo> manageSearch(Long orderNo, int pageNum, int pageSize);
+
+    ServerResponse<String> manageSendGoods(Long orderNo);
+
 }
